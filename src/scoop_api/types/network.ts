117e40856bc67c4a600895ff11f9b3a5f7a33b56
Rémy Godet <godetremy@free.fr>
2024-09-02 11:03:17 +0200

    feat(scoop): Added scoop_api_network_contact_response type to scoop api

--- src/scoop_api/types/network.ts
+++ src/scoop_api/types/network.ts
@@ -9,3 +9,34 @@ export type scoop_api_network = {
     contact_id: number,
     tos_id: number
 }
+
+export type scoop_api_network_contact_response = {
+    id: number,
+    phone: string,
+    opening_hours: string,
+    details: string,
+    created_at: string,
+    updated_at: string,
+    network_id: number,
+    webview: string
+}
+
+export type scoop_api_network_contact = {
+    id: number,
+    phone: string,
+    opening_hours: {
+        html: string,
+        text: string,
+    },
+    details: {
+        html: string,
+        text: string,
+    },
+    created_at: Date,
+    updated_at: Date,
+    network_id: number,
+    footer: {
+        html: string,
+        text: string,
+    }
+}
