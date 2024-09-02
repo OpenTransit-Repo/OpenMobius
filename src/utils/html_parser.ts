dbbf9745cd08b450132002293fe544be274fd1de
Rémy Godet <godetremy@free.fr>
2024-09-02 11:11:04 +0200

    feat(utils): Updated html_parser.ts

--- src/utils/html_parser.ts
+++ src/utils/html_parser.ts
@@ -1,3 +1,10 @@
 export function html_to_text(html) {
-    return html.replace(/<[^>]*>/g, '');
+    return html
+        .replace(/<style([\s\S]*?)<\/style>/gi, '')
+        .replaceAll('<br>', '\n')
+        .replace(/<[^>]*>/g, '\n')
+        .split('\n')
+        .filter((x) => x.trim() !== '')
+        .join('\n')
+        .trim();
 }
